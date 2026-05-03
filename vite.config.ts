86f2e1fdc64ec24b0849cde7cf90e9379f09c424
kispekorea <kispekorea@gmail.com>
2026-05-03 22:40:42 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/daymode/', // 이 줄이 있어야 깃허브 주소 경로를 제대로 인식합니다!
-})
+  // 1. 저장소 이름이 'daymode'가 맞다면 반드시 이대로!
+  base: '/daymode/', 
+  
+  plugins: [react(), tailwindcss()],
+  
+  resolve: {
+    alias: {
+      // 2. 경로 인식을 위해 필수
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  
+  build: {
+    // 3. 빌드 결과물이 나올 폴더 이름 확인
+    outDir: 'dist',
+  }
+});
