056752d8589d3c6c2ff2315b6005093d8269942b
kispekorea <kispekorea@gmail.com>
2026-05-03 22:13:39 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-daymode-app/', // 이 줄이 있어야 깃허브 주소 경로를 제대로 인식합니다!
+  base: '/daymode/', // 이 줄이 있어야 깃허브 주소 경로를 제대로 인식합니다!
 })
